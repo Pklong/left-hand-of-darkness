626c0684871f1cc95ce01e0c7c368462abc8f721
PKLong <patrickklong@gmail.com>
2017-10-09 22:41:06 -0400

    add form for user auth

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 import React, { Component } from "react"
 import reactDOM from "react-dom"
+import config from "./config"
 
-const App = () => <h1>Hello World</h1>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.fetchGithub = this.fetchGithub.bind(this)
+  }
+
+  setChange(field) {
+    return e => {
+      this.setState({ [field]: e.target.value })
+    }
+  }
+  fetchGithub(e) {
+    e.preventDefault()
+    fetch("https://api.github.com/zen")
+      .then(res => res.text())
+      .then(data => console.log(data))
+  }
+  render() {
+    return (
+      <form onSubmit={this.fetchGithub}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          onChange={this.setChange("username")}
+          value={this.state.username}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={this.setChange("password")}
+          value={this.state.password}
+        />
+        <input type="submit" value="Access Github" />
+      </form>
+    )
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.querySelector("#root")
