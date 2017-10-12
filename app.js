ff59db9c4f62640591c2cc559e0d711da2ab43be
PKLong <patrickklong@gmail.com>
2017-10-11 22:52:40 -0400

    remove form

--- app.js
+++ app.js
@@ -5,43 +5,15 @@ import config from "./config"
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      username: "",
-      password: ""
-    }
-    this.fetchGithub = this.fetchGithub.bind(this)
-  }
-
-  setChange(field) {
-    return e => {
-      this.setState({ [field]: e.target.value })
-    }
-  }
-  fetchGithub(e) {
-    e.preventDefault()
-    fetch("https://api.github.com/zen")
-      .then(res => res.text())
-      .then(data => console.log(data))
   }
   render() {
+    const queryString = `?client_id=${config.Client_ID}&scope=public_repo`
     return (
-      <form onSubmit={this.fetchGithub}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={this.setChange("username")}
-          value={this.state.username}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          onChange={this.setChange("password")}
-          value={this.state.password}
-        />
-        <input type="submit" value="Access Github" />
-      </form>
+      <div>
+        <a href={`https://github.com/login/oauth/authorize${queryString}`}>
+          Login
+        </a>
+      </div>
     )
   }
 }
