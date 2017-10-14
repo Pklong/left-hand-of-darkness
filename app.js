367a6e58092dd574e7372083d1deaa5ba686417d
PKLong <patrickklong@gmail.com>
2017-10-13 23:26:45 -0400

    display username and avatar

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react'
-import reactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import config from './config'
+import React, { Component } from "react"
+import reactDOM from "react-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom"
+import config from "./config"
 
 class DisplayGithub extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      repos: []
+      user: null
     }
   }
 
   componentDidMount() {
-    fetch()
+    fetch("https://api.github.com/user?access_token=" + this.props.token)
+      .then(u => u.json())
+      .then(d => this.setState({ user: d }))
+  }
+
+  render() {
+    if (this.state.user === null) {
+      return false
+    }
+    const { avatar_url, login } = this.state.user
+
+    return (
+      <article>
+        <h1>{login}</h1>
+        <img src={avatar_url} />
+      </article>
+    )
   }
 }
 
@@ -25,27 +46,43 @@ class FetchGithub extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/auth?code=' + this.props.code)
+    fetch("http://localhost:8080/auth?code=" + this.props.code)
       .then(d => d.text())
       .then(accessToken => this.setState({ accessToken }))
       .catch(err => console.error(err))
   }
   render() {
-    return <div>{this.state.accessToken}</div>
+    if (this.state.accessToken) {
+      return <DisplayGithub token={this.state.accessToken} />
+    } else {
+      return null
+    }
   }
+}
+
+const Auth = ({ component: Component, path, location: { search } }) => {
+  return (
+    <Route
+      path={path}
+      render={props =>
+        search.startsWith("?code") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )}
+    />
+  )
 }
 
 const App = ({ location: { search } }) => {
   const queryString = `?client_id=${config.Client_ID}&scope=public_repo`
-  if (search) {
-    return <FetchGithub code={search.split('=')[1]} />
+  if (search.startsWith("?code")) {
+    return <FetchGithub code={search.split("=")[1]} />
   } else {
     return (
-      <div>
-        <a href={`https://github.com/login/oauth/authorize${queryString}`}>
-          Login
-        </a>
-      </div>
+      <a href={`https://github.com/login/oauth/authorize${queryString}`}>
+        Login
+      </a>
     )
   }
 }
@@ -61,8 +98,8 @@ const Root = () => {
   )
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.querySelector('#root')
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.querySelector("#root")
   reactDOM.render(<Root />, root)
 })
 
