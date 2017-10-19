67f75182dc942d274a8cf34c2b2bbb4ae309253c
PKLong <patrickklong@gmail.com>
2017-10-18 21:59:35 -0400

    pull client id from config

--- components/login.js
+++ components/login.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Client_ID } from "../util/api"
+import { Client_ID } from "../util/config"
 
 const Login = () => {
   const queryString = `?client_id=${Client_ID}&scope=public_repo`
