1f071a28e5fc99c1f0a7d04d0fb00a4b32ecee55
PKLong <patrickklong@gmail.com>
2017-11-11 12:50:58 -0500

    Add object spread

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["react", "env"],
+            plugins: ["transform-object-rest-spread"],
             env: {
               production: {
                 presets: ["minify"]
