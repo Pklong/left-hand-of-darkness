b630404dc8859275a34e30a7d3b23eb366f75341
PKLong <patrickklong@gmail.com>
2017-11-12 18:39:59 -0500

    remove nonesense from auth route, smh

--- components/util/auth_route.js
+++ components/util/auth_route.js
@@ -3,9 +3,6 @@ import { connect } from "react-redux"
 import { Redirect, Route, withRouter } from "react-router-dom"
 
 const AuthRoute = ({ component, loggedIn, ...props }) => {
-  const pathMatch = props.path === props.location.pathname
-  if (!pathMatch) return null
-
   if (loggedIn) {
     return <Route {...props} component={component} />
   } else {
